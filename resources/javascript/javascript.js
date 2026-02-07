062d94dce90228fb842e67fe7e9a5b501eb496ed
thanasimiora <a.n.kontogeorgos@gmail.com>
2026-02-07 20:52:45 +0200

    Add files via upload

---
+++ resources/javascript/javascript.js
@@ -0,0 +1,230 @@
+var articles = [
+  {
+    id: 19,
+    date: "7/2/2026",
+    title: "Απάντηση στους Άσχετους- Ανάλυση των επών του Πύθουλα #1",
+    pic: "pith1",
+    category: "ΠΟΛΙΤΙΣΜΟΣ",
+    author: "ROBERT MUGABE jr",
+    link: "pith1",
+    text: " Lorem ipsum",
+  },
+  {
+    id: 18,
+    date: "6/2/2026",
+    title: "Κινέζοι κατάσκοποι και στο ζωικό βασίλειο",
+    pic: "lemourioi",
+    category: "ΠΟΛΙΤΙΣΜΟΣ",
+    author: "ROBERT MUGABE jr",
+    link: "lemourioi",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 17,
+    date: "6/2/2026",
+    title: "Σπείρα της υποταγής συνειδήσεων: ο κύκλος θανάτου και κυτταρίνης",
+    pic: "xarti",
+    category: "ΤΕΧΝΟΛΟΓΙΑ",
+    author: "ΑΓΑΘΟΚΛΗ",
+    link: "xarti",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 16,
+    date: "6/2/2026",
+    title:
+      "Εσφιγμένου: Η εκκλησιαστική σύγκρουση που διχάζει την Ορθοδοξία – Κατηγορίες, βία και μια πληγή 50 ετών",
+    pic: "moni",
+    category: "ΠΟΛΙΤΙΣΜΟΣ",
+    author: "Father Diablo",
+    link: "moni",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 15,
+    date: "5/2/2026",
+    title:
+      "Artemis II: Το Παλαιό Σεληνιακό Συμβούλιο και η υποταγή του ανθρώπινου είδους",
+    pic: "artemis",
+    category: "ΦΥΣΙΚΗ",
+    author: "ΑΓΑΘΟΚΛΗ",
+    link: "artemis",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 14,
+    date: "5/2/2026",
+    title: "Άχαστα σημεία της Πέμπτης #3",
+    pic: "axasta3",
+    category: "ΣΤΟΙΧΗΜΑ",
+    author: "SAFARI SNIPER",
+    link: "axasta3",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 13,
+    date: "5/2/2026",
+    title:
+      "Οι Σκιώδεις Αρχιτέκτονες του Χάους: Πώς οι Τραπεζίτες και οι Ελίτ Σκηνοθέτησαν τον Πρώτο Παγκόσμιο Πόλεμο",
+    pic: "ww1",
+    category: "ΙΣΤΟΡΙΑ",
+    author: "Idi Amin Dada Oumee",
+    link: "ww1",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 12,
+    date: "4/2/2026",
+    title: "Το Κολυμβάρι, ο Οικουμενισμός και η κρίση στην σύγχρονη Ορθοδοξία",
+    pic: "cigar",
+    category: "ΠΟΛΙΤΙΣΜΟΣ",
+    author: "Father Diablo",
+    link: "cigar",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 11,
+    date: "4/2/2026",
+    title: "Άχαστα σημεία της Τετάρτης #2",
+    pic: "arubi",
+    category: "ΣΤΟΙΧΗΜΑ",
+    author: "SAFARI SNIPER",
+    link: "arubi",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 10,
+    date: "4/2/2026",
+    title: "Νέα Τάξη πραγμάτων και οικογενειακή δυστυχία",
+    pic: "diazigia",
+    category: "ΟΙΚΟΝΟΜΙΑ",
+    author: "ROBERT MUGABE jr",
+    link: "diazigia",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 9,
+    date: "3/2/2026",
+    title: "O Σμαραγδιζων Σιδηρούν Ήλιος",
+    pic: "ilios",
+    category: "ΦΥΣΙΚΗ",
+    author: "ROBERT MUGABE jr",
+    link: "ilios",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 8,
+    date: "3/2/2026",
+    title: 'Ήταν Εβραίος ο "Μέγας" Κωνσταντίνος?',
+    pic: "konnos",
+    category: "ΙΣΤΟΡΙΑ",
+    author: "ROBERT MUGABE jr",
+    link: "konnos",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 7,
+    date: "3/2/2026",
+    title: '"Ελλειπείς" ελλείψεις και "τέλειοι" κύκλοι',
+    pic: "ellipseis",
+    category: "ΦΥΣΙΚΗ",
+    author: "ROBERT MUGABE jr",
+    link: "ellipseis",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 6,
+    date: "3/2/2026",
+    title: "Ιστορική καταγωγή των πάντων (και του T-Rex)",
+    pic: "dinosaur",
+    category: "ΦΥΣΙΚΗ",
+    author: "ROBERT MUGABE jr",
+    link: "dinosaur",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 5,
+    date: "1/2/2026",
+    title: "Μας τα λεγε ο Μελ απ' το 2002!!!",
+    pic: "faraday",
+    category: "ΦΥΣΙΚΗ",
+    author: "ROBERT MUGABE jr",
+    link: "faraday",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 4,
+    date: "1/2/2026",
+    title: "Άχαστα σημεία της Κυριακής #1",
+    pic: "axasta",
+    category: "ΣΤΟΙΧΗΜΑ",
+    author: "SAFARI SNIPER",
+    link: "axasta",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 3,
+    date: "1/2/2026",
+    title: "Γενετικά πειράματα στη Μινωική Κρήτη???",
+    pic: "genetika",
+    category: "ΙΣΤΟΡΙΑ",
+    author: "ROBERT MUGABE jr",
+    link: "genetika",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 2,
+    date: "1/2/2026",
+    title: "Δυτική μουσική-Από τον Μπαχ στον Πύθουλα",
+    pic: "mousiki",
+    category: "ΙΣ22ΤΟΡΙΑ",
+    author: "ROBERT MUGABE jr",
+    link: "mousiki",
+    text: " Lore22m ipsum",
+  },
+  {
+    id: 1,
+    date: "1/2/2026",
+    title: "Αφρική το νέο Ελντοράντο;",
+    pic: "eldorado",
+    category: "ΣΤΟΙΧΗΜΑ",
+    author: "SAFARI SNIPER",
+    link: "eldorado",
+    text: " Lore22m ipsum",
+  },
+];
+
+function combineTexts(articles) {
+  let result = "";
+
+  articles.forEach((article) => {
+    result +=
+      '<div class="newscontainer"><div><div class="newsdiv"><a href="temp/' +
+      article.link +
+      '.html"><img src="resources/images/' +
+      article.pic +
+      '.png" alt="article"/></a><h6 class="category"> ' +
+      article.category +
+      '</h6></div><h5 id="date">' +
+      article.date +
+      '</h5></div><a href="temp/' +
+      article.link +
+      '.html"><h1 id="title">' +
+      article.title +
+      "</h1></a></div>";
+  });
+  return result.trim();
+}
+
+const newz = combineTexts(articles);
+
+document.getElementById("news").innerHTML = newz;
+
+document.getElementById("barr").innerHTML =
+  '<div><button id="menu" data-bs-toggle="dropdown">☰</button><ul id="menu"class="dropdown-menu"><li><a class="dropdown-item" href="istoria.html">ΙΣΤΟΡΙΑ</a></li><li><a class="dropdown-item" href="fisiki.html">ΦΥΣΙΚΗ</a></li><li><a class="dropdown-item" href="politismos.html">ΠΟΛΙΤΙΣΜΟΣ</a></li><li><a class="dropdown-item" href="texnologia.html">ΤΕΧΝΟΛΟΓΙΑ</a></li><li><a class="dropdown-item" href="oikonomia.html">ΟΙΚΟΝΟΜΙΑ</a></li><li><a class="dropdown-item" href="stoixima.html">ΣΤΟΙΧΗΜΑ</a></li></ul></div><div id=logo><a href="index.html"><img id=logo src="resources/images/logo.png" alt="logo" width="100%" height="50%"></a></div><div><ul class="sec_colornav col-md-4 justify-content-end list-unstyled d-flex" ><li  class="ms-3"><a class="text-body-secondary" href="#"><svg id="social" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" text1 bi bi-twitter-x " viewBox="0 0 16 16"><path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/></svg></a></li><li class="ms-3"><a class="text-body-secondary" href="#"><svg  id="social" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" text1 bi bi-instagram" viewBox="0 0 16 16"><path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/></svg></a></li><li class="ms-3"><a class="text-body-secondary" href="#"><svg  id="social" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" text1 bi bi-facebook" viewBox="0 0 16 16"><path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/></svg></use></svg></a></li></ul></div>';
+
+document.getElementById("crypto_container").innerHTML =
+  '<div class="crypto"><gecko-coin-price-static-headline-widget locale="en"    outlined="false" coin-ids="popcat,simon-s-cat, poocoin,pepe,floki,dogecoin,hamster-kombat,shiba-inu,pepe-trump"  initial-currency="usd"></gecko-coin-price-static-headline-widget>  </div> <div aria-hidden class="crypto"> <gecko-coin-price-static-headline-widget  locale="en"  outlined="false"  coin-ids="popcat,poocoin,pepe,floki,dogecoin,hamster-kombat,shiba-inu,pepe-trump"  initial-currency="usd" ></gecko-coin-price-static-headline-widget>  </div>';
+
+document.getElementById("footer").innerHTML =
+  '<div  class="col-md-4 d-flex align-items-center"><h5>ΟΡΟΙ ΧΡΗΣΗΣ</h5><img  src="resources/images/logo_footer.png" alt="logo" width="100%" height="50%"><h5>ΕΠΙΚΟΙΝΩΝΙΑ</h5></div>';
